2bff8a690242e741729f8bcbecbca3bad3e88843
Abhishek Srivastava <avix1011.a@gmail.com>
2025-08-13 18:39:17 +0530

    src

--- loop-back/src/index.ts
+++ loop-back/src/index.ts
@@ -1,6 +1,6 @@
 import {StarterApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
-
+export * from './application';
 export {StarterApplication};
 
 export async function main(options: ApplicationConfig = {}) {
@@ -13,4 +13,18 @@ export async function main(options: ApplicationConfig = {}) {
   console.log(`Try ${url}/ping`);
 
   return app;
+}
+if (require.main === module) {
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? 3000),
+      host: process.env.HOST ?? 'localhost',
+      openApiSpec: {setServersFromRequest: true},
+    },
+  };
+
+  main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
 }
